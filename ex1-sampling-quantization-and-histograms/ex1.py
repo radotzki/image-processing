b0d520222bd1af4489dcb8525e47dc19cbd1d5a0
Itay Radotzki <radotzki@gmail.com>
2017-11-23 00:32:36 -0800

    final

--- ex1-sampling-quantization-and-histograms/ex1.py
+++ ex1-sampling-quantization-and-histograms/ex1.py
@@ -5,20 +5,19 @@ def getSampledImageAtResolution(dim, pixel_size, k=2):
     y = np.linspace(dim[2], dim[3], (dim[3] - dim[2]) / pixel_size)
     X, Y = np.meshgrid(x, y)
     I = np.cos(k * np.pi * (3 * X + 2 * Y))
+
     return I
+
 
 def optimalQuantizationImage(img, k):
     z = []
-    for i in range(0, 32):
+    for i in range(0, k):
         z.append(np.uint8(i * 256 / k))
     z.append(255)
 
     q = np.zeros(k)
     q_prev = np.ones(k)
-    img_histogram = np.zeros(256)
-
-    for i in range(0, 255):
-        img_histogram[i] = np.sum(img == i)
+    img_histogram = getImageHistogram(img)
 
     while np.sum(np.abs(q - q_prev)) > 0.1:
         q_prev = np.copy(q)
@@ -45,3 +44,34 @@ def optimalQuantizationImage(img, k):
             result[row,col] = np.uint8(hash_table[pixel])
 
     return result
+
+
+def getImageHistogram(img):
+    hist = np.zeros(256)
+    for i in range(0, 255):
+        hist[i] = np.sum(img == i)
+
+    return hist
+
+
+def getConstrastStrechedImage(img):
+    minVal = img.min()
+    maxVal = img.max()
+    img = (img - minVal) / (maxVal - minVal)
+    img = img * 255
+
+    return np.uint8(img)
+
+
+def getHistEqImage(img):
+    hist = getImageHistogram(img)
+    alpha = 255 / hist.sum()
+    hash_table = np.zeros(256)
+    hash_table[0] = alpha * hist[0]
+    for i in np.arange(1, 256):
+        hash_table[i] = np.floor(hash_table[i - 1] + alpha * hist[i])
+
+    res = hash_table[img]
+    res = np.uint8(res)
+
+    return res
